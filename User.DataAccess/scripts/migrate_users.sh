81f6631ff355a96bc7074bc5cc29f07ce48e77ca
KevinBacon <turnipsh@student.21-school.ru>
2024-05-26 17:18:49 +0300

    delete user access class library (couse is transfering it to taskManager.Access)

--- User.DataAccess/scripts/migrate_users.sh
+++
@@ -1,5 +0,0 @@
-dotnet ef migrations add init --context UserDbContext  -s .\TaskManager.API\ -p .\User.DataAccess\
- 
-dotnet ef database update  --context UserDbContext -s .\TaskManager.API\ -p .\User.DataAccess\
-
-
